b18c56d2952c76b5a20ab6b5bee05b1bd4588f71
ruhan2000 <ruhan.behlim@aminsofttech.co>
2025-05-24 18:59:29 +0530

    port change

--- app.js
+++ app.js
@@ -253,7 +253,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3002;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
