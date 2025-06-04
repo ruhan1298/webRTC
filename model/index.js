173a3bbcc65c5f25278a9491357f07174e263905
ruhan2000 <ruhan.behlim@aminsofttech.co>
2025-06-04 17:22:12 +0530

    chat

--- model/index.js
+++ model/index.js
@@ -1,41 +1,41 @@
-// const { Sequelize, DataTypes } = require('sequelize');
-
-// const sequelize = new Sequelize('webrtc_9ecp', 'webrtcuser', 'Hp8OJSY9UfH1IKlFAQ1ttoKQ24XIjKro', {
-//   host:'dpg-d0ot65je5dus73d89vig-a',
-//   dialect: 'postgres',   // yahan dialect ko 'postgres' kar diya
-//   logging: false,
-//   port: 5432             // agar port change nahi kiya toh default 5432 hota hai
-// });
-
-// (async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Database Connected successfully.');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-// })();
-
-// module.exports = sequelize;
-
-
-
-
 const { Sequelize, DataTypes } = require('sequelize');
 
+const sequelize = new Sequelize('webrtc_9ecp', 'webrtcuser', 'Hp8OJSY9UfH1IKlFAQ1ttoKQ24XIjKro', {
+  host:'dpg-d0ot65je5dus73d89vig-a',
+  dialect: 'postgres',   // yahan dialect ko 'postgres' kar diya
+  logging: false,
+  port: 5432             // agar port change nahi kiya toh default 5432 hota hai
+});
 
-
-const sequelize = new Sequelize('webrtc','root', 'Ruhan@4430', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
-  });
-
+(async () => {
   try {
-     sequelize.authenticate();
+    await sequelize.authenticate();
     console.log('Database Connected successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
+})();
+
+module.exports = sequelize;
+
+
+
+
+// const { Sequelize, DataTypes } = require('sequelize');
+
+
+
+// const sequelize = new Sequelize('webrtc','root', 'Ruhan@4430', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     logging: false
+//   });
+
+//   try {
+//      sequelize.authenticate();
+//     console.log('Database Connected successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
   
-  module.exports=sequelize
+//   module.exports=sequelize
